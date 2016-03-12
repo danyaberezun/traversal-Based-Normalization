216ff9d4bf84692eb65098341c5f74d4e890f898
Dmitry Boulytchev <dboulytchev@gmail.com>
2016-03-13 00:30:30 +0300

    Improved CHLR terms

--- coq/CHLR.v
+++ coq/CHLR.v
@@ -1,5 +1,7 @@
 Require Import List.
 Import ListNotations.
+Require Import Omega.
+
 
 (* Role of a subtree w.r.t. its parent node: whether
    it its unique descendant (U), right (R) or left (L)
@@ -12,44 +14,52 @@ Inductive Role := U | R | L.
 Definition Path := list Role.
 
 (* A term tree is a tree with each node indexed by
-   a path
+   a path and number of enclosing lambdas
 *)
-Inductive TermT (P : Path) := 
-  Lam  : TermT (P++[U]) -> TermT P
-| App  : TermT (P++[L]) -> TermT (P++[R]) -> TermT P
-| BVar : Path -> TermT P
-| FVar : nat  -> TermT P.
+Inductive TermT (P : Path) (l : nat) := 
+  Lam  : TermT (P++[U]) (S l) -> TermT P l
+| App  : TermT (P++[L]) l -> TermT (P++[R]) l -> TermT P l
+| BVar : forall (n : nat), n < l -> TermT P l
+| FVar : nat -> TermT P l.
 
 (* A term is a tree with the root indexed by empty
    path
 *)
-Definition Term := TermT [].
+Definition Term := TermT [] 0.
 
 (* Some test cases *)
-Definition IdT  : Term := Lam [] (BVar [U] []).
-Definition AppT : Term := Lam [] (Lam [U] (App [U; U] (BVar [U;U;L] []) (BVar [U;U;R] [U]))).
+Definition IdT  : Term.
+  refine (Lam [] 0 (BVar [U] 1 0 _)). 
+  omega.
+Defined.
+
+Definition AppT : Term. 
+  refine (Lam [] 0 (Lam [U] 1 (App [U; U] 2 (BVar [U;U;L] 2 1 _) (BVar [U;U;R] 2 0 _)))).
+  omega. omega.
+Defined.
 
 Lemma cons_to_app : forall (A : Set) (a : A) (l : list A), a::l = [a] ++ l.
 Proof.
   intros A a l. reflexivity.
 Qed.
 
-Definition subterm : forall (P P' : Path), TermT P -> option (TermT (P ++ P')).
+Definition subterm : forall (P P' : Path) (l : nat), TermT P l -> option (TermT (P ++ P') l').
   refine (
-    fix subterm' (P P' : Path) {struct P'} : TermT P -> option (TermT (P ++ P')) := fun T =>
-      match P' return option (TermT (P ++ P')) with
+    fix subterm' (P P' : Path) 
+                 (l : nat ) {struct P'} : TermT P l -> exists l', option (TermT (P ++ P') l') := fun T =>
+      match P' return exists l', option (TermT (P ++ P') l') with
       | []   => Some _
       | x::p =>           
-          match x return option (TermT (P ++ x::p)) with
-          | U => match T return option (TermT (P ++ U::p)) with
+          match x return exists l', option (TermT (P ++ x::p) l') with
+          | U => match T return exists l', option (TermT (P ++ U::p) l') with
                  | Lam t => _
                  | _     => None
                  end 
-          | L => match T return option (TermT (P ++ L::p)) with
+          | L => match T return exists l', option (TermT (P ++ L::p) l') with
                  | App t _ => _
                  | _       => None
                  end 
-          | R => match T return option (TermT (P ++ R::p)) with
+          | R => match T return exists l', option (TermT (P ++ R::p) l') with
                  | App _ t => _
                  | _       => None
                  end 
