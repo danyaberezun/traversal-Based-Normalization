151c4275d898a5e5e3579d754f308915993a8548
danyaberezun <danya.berezun@gmail.com>
2016-06-08 16:25:42 +0300

    substitution function is added

--- coq/CHLR.v
+++ coq/CHLR.v
@@ -35,53 +35,414 @@ Defined.
 
 Definition AppT : Term. 
   refine (
-    Lam [] 0 (
-      Lam [U] 1 (
-        App [U; U] 2 
-          (BVar [U;U;L] 2 1 _) 
-          (BVar [U;U;R] 2 0 _)
-      )
-    )
-  ).
+      Lam [] 0 (
+            Lam [U] 1 (
+                  App [U; U] 2 
+                      (BVar [U;U;L] 2 1 _) 
+                      (BVar [U;U;R] 2 0 _)
+                )
+          )
+    ).
   omega. omega.
 Defined.
 
+Definition AppT' : Term. 
+  refine (
+      Lam [] 0 (
+            Lam [U] 1 (
+                  App [U; U] 2 
+                      (BVar [U;U;L] 2 0 _) 
+                      (BVar [U;U;R] 2 0 _)
+                )
+          )
+    ).
+  omega. omega.
+Defined.
+
+Eval compute in AppT'.
+Eval compute in AppT.
+
 (* Auxilliary lemma *)
 Lemma cons_to_app : forall (A : Set) (a : A) (l : list A), a::l = [a] ++ l.
-Proof.
-  intros A a l. reflexivity.
-Qed.
+Proof. auto. Qed.
+
 
 (* Subterm: subterm t p returns a subterm of t, indexed by 
    (relative) path p; if the term does not exists, returns None
 *)
 Definition subterm : 
-  forall {P : Path} {l : nat} (P' : Path), 
+  forall {P : Path} {l : nat} (P' : Path),
   TermT P l -> option { l' : nat & TermT (P ++ P') l'}.
   refine (
-    fix subterm' {P : Path} {l : nat} (P' : Path) {struct P'} : TermT P l -> option { l' : nat & TermT (P ++ P') l'} := fun T =>
+      fix subterm' {P : Path} {l : nat} (P' : Path) {struct P'}
+      : TermT P l -> option { l' : nat & TermT (P ++ P') l'} :=
+        fun T =>
+      (*     match P' return option { l' : nat & TermT (P ++ P') l'} with *)
+      (*       | [] => Some ((existT (fun x : nat => TermT (P ++ []) x) l) *)
+      (*                       (eq_rec_r (fun l0 : list Role => TermT l0 l) T (app_nil_r P))) *)
+      (*       (* | [] => Some (existT (fun l' : nat => TermT (P ++ []) l') l) *) *)
+      (*       (*                      (eq_rec_r (fun l0 : list Role => TermT l0 l) T (app_nil_r P))) *) *)
+      (*       | _ => None *)
+      (*     end *)
+                                                                  (* ). *)
       match P' return option { l' : nat & TermT (P ++ P') l'} with
       | []   => Some _
-      | x::p =>           
+      | x::p =>
           match x return option { l' : nat & TermT (P ++ x::p) l'} with
           | U => match T return option { l' : nat & TermT (P ++ U::p) l'} with
-                 | Lam t => _
+                 | Lam _ _ t => _
                  | _     => None
-                 end 
+                 end
           | L => match T return option { l' : nat & TermT (P ++ L::p) l'} with
-                 | App t _ => _
+                 | App _ _ t _ => _
                  | _       => None
-                 end 
+                 end
           | R => match T return option { l' : nat & TermT (P ++ R::p) l'} with
-                 | App _ t => _
+                 | App _ _ _ t => _
                  | _       => None
-                 end 
-        end  
-      end). 
-  rewrite app_nil_r. exists l. exact T.  
-  apply (subterm' (P++[U]) (S l) p) in t. rewrite cons_to_app. rewrite app_assoc. assumption.
-  apply (subterm' (P++[R])  l    p) in t. rewrite cons_to_app. rewrite app_assoc. assumption.  
-  apply (subterm' (P++[L])  l    p) in t. rewrite cons_to_app. rewrite app_assoc. assumption.
+                 end
+        end
+  end).
+  exists l. rewrite app_nil_r. apply T.
+  apply (subterm' (P++[U]) (S l) p) in t. rewrite <- app_assoc in t. assumption.
+  apply (subterm' (P++[R])  l    p) in t; rewrite <- app_assoc in t; assumption.
+  apply (subterm' (P++[L])  l    p) in t; rewrite <- app_assoc in t; assumption.
 Defined.
 
 Notation "A [ p ]" := (subterm p A) (at level 0).
+
+Print subterm.
+Check subterm.
+Eval compute in (AppT) [[U;U]].
+Eval compute in (AppT) [[]].
+Eval compute in ((AppT) [[]] : option {l' : nat & TermT ([]) l'}).
+Eval cbv in (((AppT) [[]] : option {l' : nat & TermT ([]) l'})).
+
+Require Import Coq.Program.Equality.
+
+Theorem AppTEq :
+  ((AppT) [[]] : option {l' : nat & TermT ([]) l'}) =
+  Some (existT (fun x => TermT ([]) x) 0 AppT).
+Proof. simpl_eq; auto. Qed.
+
+Theorem PE :
+  option {l' : nat & TermT ([] ++ []) l'} = option {l' : nat & TermT ([]) l'}.
+Proof. reflexivity. Qed.
+
+Theorem TEq:
+  forall T : Term,
+    (T [[]] : option {l' : nat & TermT ([]) l'}) =
+    Some (existT (fun x => TermT ([]) x) 0 T).
+Proof. simpl_eq; auto. Qed.
+
+Definition laminPath :
+  Path -> nat.
+  refine(fix laminPath' (p : Path) : nat :=
+           match p with
+             | [] => 0
+             | U :: ps => 1 + laminPath' ps
+             | _ :: ps => laminPath' ps
+           end
+        ).
+Defined.
+
+(* Definition fixpath : *)
+(*   forall {P : Path} {l : nat} (P' : Path), *)
+(*     TermT P l -> TermT P' l. *)
+(*   refine( *)
+(*       fun P l P' t => *)
+(*         let fix fixpath' {P : Path} {l : nat} (t : TermT P l) (P' : Path) : TermT P' l := *)
+(*             match t with *)
+(*               | Lam t     => Lam P' l (fixpath' t (P' ++ [U])) *)
+(*               | App t1 t2 => App P' l (fixpath' t1 (P' ++ [L])) (fixpath' t1 (P' ++ [R])) *)
+(*               | BVar n eq => BVar P' l n eq *)
+(*               | FVar n    => FVar P' l n *)
+(*             end *)
+(*         in fixpath' t P' *)
+(*     ). *)
+(* Defined. *)
+
+Definition fixpath :
+  forall {P : Path} {l : nat} (P' : Path) (l' : nat),
+    TermT P l -> l' >= l ->TermT P' l'.
+  refine(
+      fun P l P' l' t EQ =>
+        let fix fixpath' {P : Path} {l : nat} (t : TermT P l) (l' : nat) (P' : Path) (pr : l' >= l)
+            : TermT P' l' :=
+            match t with
+              | Lam _ _ t     => Lam P' l' (fixpath' t (S l') (P' ++ [U]) _)
+              | App _ _ t1 t2 => App P' l' (fixpath' t1 l' (P' ++ [L]) pr) (fixpath' t1 l' (P' ++ [R]) _)
+              | BVar _ _ n eq => BVar P' l' n _
+              | FVar _ _ n    => FVar P' l' n
+            end
+        in fixpath' t l' P' EQ
+    ).
+  omega.
+  omega.
+  omega.
+Defined.
+
+(* Eval compute in AppT. *)
+(* Eval compute in fixpath ([U]) AppT. *)
+
+(* Definition substsubterm : *)
+(*   forall {P : Path} {l : nat}, *)
+(*   TermT P l -> Path -> Path -> option {l : nat & TermT P l}. *)
+(*   refine( *)
+(*       fun P l t p1 p2 => *)
+(*         let fix substsubterm' {P' p1 p2 : Path} {l' l1 l2 : nat} *)
+(*                 (p1' : Path) *)
+(*                 (t : TermT P' l') *)
+(*                 (t1 : TermT p1 l1) *)
+(*                 (T2 : TermT p2 l2) *)
+(*             : option {l : nat & TermT P' l} := *)
+(*             match p1' with *)
+(*               | [] => Some (existT (fun x => TermT P' x) l1 (fixpath P' t1)) *)
+(*               | x::xs => *)
+(*                 match x with *)
+(*                   | U => match t with *)
+(*                            | Lam t' => *)
+(*                              match (substsubterm' xs t' t1 T2) with *)
+(*                                | None => None *)
+(*                                | Some (existT l0 t0) => *)
+(*                                  Some _ (* (existT (fun x => TermT P' x) l (Lam P' (pred l0) t0)) *) *)
+(*                              end *)
+(*                            | _ => None *)
+(*                          end *)
+(*                   | L => match t with *)
+(*                            | App t' t'' => match (substsubterm' xs t' t1 T2) with *)
+(*                                              | None => None *)
+(*                                              | Some (existT l0 t0) => *)
+(*                                                Some _ *)
+(*                                            end *)
+(*                                       (* Some _ (* App P' l (substsubterm' t' t1 T2) t'' *) *) *)
+(*                            | _ => None *)
+(*                          end *)
+(*                   | R => match t with *)
+(*                            | App t' t'' => match (substsubterm' xs t'' t1 T2) with *)
+(*                                              | None => None *)
+(*                                              | Some (existT l0 t0) => *)
+(*                                                Some _ *)
+(*                                            end *)
+(*                                (* Some _ (* App P' l t' (substsubterm' t'' t1 T2) *) *) *)
+(*                            | _ => None *)
+(*                          end *)
+(*                 end *)
+(*             end *)
+(*         in let t' := t [p1] *)
+(*            in let t'' := t [p2] *)
+(*               in match t'' with *)
+(*                    | None     => None *)
+(*                    | Some (existT l'' t'') =>  *)
+(*                      match t' with *)
+(*                        | None    => None *)
+(*                        | Some (existT l' t') => substsubterm' p1 t t' t'' *)
+(*                      end *)
+(*                  end *)
+(*     ). *)
+(*   exists (pred l0). apply Lam. admit. *)
+(*   exists l. apply App. assumption. *)
+  
+(* Defined. *)
+
+Definition lamFromPath :
+  Path -> nat.
+  refine (fix lamFromPath' (P : Path) : nat :=
+            match P with
+              | [] => 0
+              | x :: xs => let l := (lamFromPath' xs)
+                           in match x with
+                                | U => S l
+                                | _ => l
+                              end
+            end
+         ).
+Defined.
+
+Require Import Bool.
+
+Definition eqq : Role -> Role -> bool.
+  refine (
+      fun r1 r2 => match r1 with
+                     | U => match r2 with
+                              | U => true
+                              | _ => false
+                            end
+                     | L => match r2 with
+                              | L => true
+                              | _ => false
+                            end
+                     | R => match r2 with
+                              | R => true
+                              | _ => false
+                            end
+                   end
+    ).
+Defined.
+
+Definition substsubterm1 :
+  forall {p p1 : Path} {l l1 : nat}
+                           (p1' : Path)
+                           (t : TermT p l)
+                           (T : TermT p1 l1)
+                           (eq : l >= l1),
+            option (TermT p l).
+  refine(
+      fix substsubterm' {p p1: Path} {l l1 : nat}
+                           (p1' : Path)
+                           (t : TermT p l)
+                           (T : TermT p1 l1)
+                           (eq : l >= l1)
+            : option (TermT p l) :=
+            match p1' with
+              | [] => Some (fixpath p l T _)
+              | x :: xs => match x with
+                             | U => match t with
+                                      | Lam _ _ t => match substsubterm' xs t T _ with
+                                                       | None => None
+                                                       | Some t => Some (Lam p l t)
+                                                     end
+                                      | _ => None
+                                    end
+                             | L => match t with
+                                      | App _ _ t1 t2 => match substsubterm' xs t1 T _ with
+                                                       | None => None
+                                                       | Some t1 => Some (App p l t1 t2)
+                                                         end
+                                      | _ => None
+                                    end
+                             | R => match t with
+                                      | App _ _ t1 t2 => match substsubterm' xs t2 T _ with
+                                                       | None => None
+                                                       | Some t2 => Some (App p l t1 t2)
+                                                         end
+                                      | _ => None
+                                    end
+                           end
+            end
+    ).
+  omega.
+  omega.
+  omega.
+  omega.
+Defined.
+
+Definition substsubterm :
+  forall {P : Path} {l : nat} (p1 p2 : Path),
+  TermT P l -> option (TermT P l).
+  refine(
+      fun P l p1 p2 t =>
+        let fix substsubterm' {p : Path} {l : nat}
+                (p1' p2' : Path)
+                (t : TermT p l)
+            : option (TermT p l) :=
+            match p2' with
+              | [] => None
+              | [R] => match p1' with
+                         | L :: ys => match t with
+                                        | App _ _ t1 t2 => match substsubterm1 ys t1 t2 _ with
+                                                             | None => None
+                                                             | Some t1 => Some (App p l t1 t2)
+                                                           end
+                                        | _ => None
+                                      end
+                         | _ => None
+                       end
+              | [L] => None
+              | [U] => None
+              | x :: xs => match p1' with
+                             | [] => None
+                             | y :: ys => match eqq x y with
+                                            | true => match x with
+                                                 | U => match t with
+                                                          | Lam _ _ t' => match (substsubterm' xs ys t') with
+                                                                            | None => None
+                                                                            | Some t' => Some (Lam p l t')
+                                                                          end
+                                                          | _ => None
+                                                        end
+                                                 | L =>  match t with
+                                                          | App _ _ t' t'' => match (substsubterm' xs ys t') with
+                                                                                | None => None
+                                                                                | Some t' => Some (App p l t' t'')
+                                                                              end
+                                                          | _ => None
+                                                        end
+                                                 | R =>  match t with
+                                                           | App _ _ t' t'' => match (substsubterm' xs ys t'') with
+                                                                                 | None => None
+                                                                                 | Some t'' => Some (App p l t' t'')
+                                                                               end
+                                                          | _ => None
+                                                         end
+                                                         end
+                                            | false => None
+                                          end
+                           end
+            end
+        in substsubterm' p1 p2 t
+    ).
+  auto.   
+Defined.
+
+Eval compute in AppT.
+Eval compute in substsubterm ([U;U;L]) ([U;U;R]) AppT.
+
+Definition TermTEquality :
+  forall {P : Path} {l : nat},
+    TermT P l -> TermT P l -> bool.
+  refine(
+      fix eq  {P : Path} {l : nat}
+          (t1 t2 : TermT P l)
+      : bool :=
+        match t1 with
+          | Lam _ _ t1 => match t2 with
+                            | Lam _ _ t2 => eq t1 t2
+                            | _ => false
+                          end
+          | App  _ _ t1' t1'' => match t2 with
+                            | App _ _ t2' t2'' => eq t1' t2' && eq t1'' t2''
+                            | _ => false
+                          end
+          | BVar _ _ n _ => match t2 with
+                            | BVar _ _ m _ => beq_nat m n
+                            | _ => false
+                          end
+          | FVar _ _ n => match t2 with
+                            | FVar _ _ m => beq_nat m n
+                            | _ => false
+                          end
+        end
+    ).
+Defined.
+
+Definition OptionTermTEquality :
+  forall {P : Path} {l : nat},
+    option (TermT P l) -> option (TermT P l) -> bool.
+  refine(
+      fix eq  {P : Path} {l : nat}
+          (t1 t2 : option (TermT P l))
+      : bool :=
+        match t1 with
+          | None => match t2 with
+                      | None => true
+                      | _ => false
+                    end
+          | Some t1 => match t2 with
+                      | None => false
+                      | Some t2 => TermTEquality t1 t2
+                    end
+        end
+    ).
+Defined.
+
+Eval cbv in   OptionTermTEquality (substsubterm ([U;U;L]) ([U;U;R]) AppT) (Some AppT').
+
+Eval compute in AppT'.
+Eval compute in AppT.
+
+Theorem substExample :
+  OptionTermTEquality (substsubterm ([U;U;L]) ([U;U;R]) AppT) (Some AppT') = true.
+Proof.
+  unfold substsubterm; unfold substsubterm1; unfold fixpath; unfold eqq;  simpl_eq; auto.
+Qed.
